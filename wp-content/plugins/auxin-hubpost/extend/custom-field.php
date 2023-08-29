67623f09aa5a2ce9b03b0bcb8dc4b2676a6f61d9
le.pham <phamle21@gmail.com>
2023-08-30 05:27:50 +0700

    validation meta box

--- wp-content/plugins/auxin-hubpost/extend/custom-field.php
+++ wp-content/plugins/auxin-hubpost/extend/custom-field.php
@@ -7,44 +7,39 @@ class Extend_Custom_Field_Hubpost
         // Add metabox
         add_action('add_meta_boxes', [$this, 'custom_hubpost_metabox']);
 
-        // Init custom field
-        add_action('init', [$this, 'add_hubpost_custom_fields']);
-
-        // Register custom field in rest
-        add_action('rest_api_init', [$this, 'register_custom_fields']);
-
         // Add bootstrap to admin page 
         add_action('admin_enqueue_scripts', [$this, 'enqueue_bootstrap_admin']);
 
         // Save
-        add_action('save_post', [$this, 'save_hubpost_custom_fields'], 20);
+        add_action('save_post', [$this, 'save_hubpost_custom_fields']);
     }
-
     function save_hubpost_custom_fields($post_id)
     {
-        // Check post type
+        if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) {
+            return;
+        }
+
         if (get_post_type($post_id) !== 'hubpost') {
             return;
         }
 
-        // Fields
-        $name = sanitize_text_field($_POST['name']);
-        $account_name = sanitize_text_field($_POST['account_name']);
-        $major = sanitize_text_field($_POST['major']);
-        $phone = sanitize_text_field($_POST['phone']);
-
-        // Check validation
-        if (empty($name)) {
-            // Noti
-            wp_die('Please fill in the blank fields. (Name field)');
+        if (isset($_POST['name'])) {
+            update_post_meta($post_id, 'name', sanitize_text_field($_POST['name']));
         }
 
-        // Lưu dữ liệu vào các trường tùy chỉnh
-        update_post_meta($post_id, 'name', $name);
-        update_post_meta($post_id, 'account_name', $account_name);
-        update_post_meta($post_id, 'major', $major);
-        update_post_meta($post_id, 'phone', $phone);
+        if (isset($_POST['account_name'])) {
+            update_post_meta($post_id, 'account_name', sanitize_text_field($_POST['account_name']));
+        }
+
+        if (isset($_POST['major'])) {
+            update_post_meta($post_id, 'major', sanitize_text_field($_POST['major']));
+        }
+
+        if (isset($_POST['phone'])) {
+            update_post_meta($post_id, 'phone', sanitize_text_field($_POST['phone']));
+        }
     }
+
     function enqueue_bootstrap_admin()
     {
         global $post_type;
@@ -69,95 +64,48 @@ class Extend_Custom_Field_Hubpost
 
     function custom_hubpost_metabox_callback()
     {
+        $name = get_post_meta(get_the_ID(), 'name', true);
+        $account_name = get_post_meta(get_the_ID(), 'account_name', true);
+        $major = get_post_meta(get_the_ID(), 'major', true);
+        $phone = get_post_meta(get_the_ID(), 'phone', true);
+
         echo '
         <div class="row">
-            <div class="col-md-6">
-                <label for="name" class="fw-bold">Name <span class="text-danger">*</span></label>
-                <input id="name" class="form-control" name="name" required>
-                <span id="error_name" class="text-danger"></span>
-            </div>
-            <div class="col-md-6">
-                <label for="account_name" class="fw-bold">Account name</label>
-                <input id="account_name" class="form-control" name="account_name">
-            </div>
-            <div class="col-md-6">
-                <label for="major" class="fw-bold">Major</label>
-                <input id="major" class="form-control" name="major">
-            </div>
-            <div class="col-md-6">
-                <label for="phone" class="fw-bold">Phone</label>
-                <input id="phone" class="form-control" name="phone">
-                <span id="error_phone" class="text-danger"></span>
-            </div>
-        </div>';
-    }
+                <div class="col-md-6">
+                    <label for="name" class="fw-bold">Name <span class="text-danger">*</span></label>
+                    <input id="hubpost-cf-name" class="form-control" name="name" value="' . $name . '">
+                    <span id="error_name" class="text-danger"></span>
+                </div>
+                <div class="col-md-6">
+                    <label for="account_name" class="fw-bold">Account name</label>
+                    <input id="account_name" class="form-control" name="account_name" value="' . $account_name . '">
+                </div>
+                <div class="col-md-6">
+                    <label for="major" class="fw-bold">Major</label>
+                    <input id="major" class="form-control" name="major" value="' . $major . '">
+                </div>
+                <div class="col-md-6">
+                    <label for="phone" class="fw-bold">Phone</label>
+                    <input id="phone" class="form-control" name="phone" value="' . $phone . '">
+                    <span id="error_phone" class="text-danger"></span>
+                </div>
+        </div>
+        <script>
+            if(jQuery("#hubpost-cf-name").val() == ""){
+                jQuery(".editor-post-publish-button").prop("disabled", true);
+            }
+            jQuery("#hubpost-cf-name").on("keyup",function(){
+                if(jQuery(this).val() == ""){
+                    jQuery(".editor-post-publish-button").prop("disabled", true);
+                    jQuery("#error_name").html("Please fill in the blank fields.");
+                }else{
+                    jQuery(".editor-post-publish-button").prop("disabled", false);
+                    jQuery("#error_name").html("");
 
-    function get_hubpost_custom_fields($object, $field_name, $request)
-    {
-        return get_post_meta($object['id'], $field_name, true);
-    }
-
-    function register_custom_fields()
-    {
-        register_rest_field('hubpost', 'name', array(
-            'get_callback' => [$this, 'get_hubpost_custom_fields'],
-            'schema' => null,
-        ));
-
-        register_rest_field('hubpost', 'account_name', array(
-            'get_callback' => [$this, 'get_hubpost_custom_fields'],
-            'schema' => null,
-        ));
-
-        register_rest_field('hubpost', 'major', array(
-            'get_callback' => [$this, 'get_hubpost_custom_fields'],
-            'schema' => null,
-        ));
-
-        register_rest_field('hubpost', 'phone', array(
-            'get_callback' => [$this, 'get_hubpost_custom_fields'],
-            'schema' => null,
-        ));
-    }
-
-    function can_user_edit_hubpost($post, $request)
-    {
-        return current_user_can('edit_post', $post['id']);
-    }
-
-    function add_hubpost_custom_fields()
-    {
-        register_post_meta('hubpost', 'name', array(
-            'type' => 'string',
-            'single' => true,
-            'show_in_rest' => true,
-            'sanitize_callback' => 'sanitize_text_field',
-            'auth_callback' => [$this, 'can_user_edit_hubpost']
-        ));
-
-        register_post_meta('hubpost', 'account_name', array(
-            'type' => 'string',
-            'single' => true,
-            'show_in_rest' => true,
-            'sanitize_callback' => 'sanitize_text_field',
-            'auth_callback' => [$this, 'can_user_edit_hubpost']
-        ));
-
-        register_post_meta('hubpost', 'major', array(
-            'type' => 'string',
-            'single' => true,
-            'show_in_rest' => true,
-            'sanitize_callback' => 'sanitize_text_field',
-            'auth_callback' => [$this, 'can_user_edit_hubpost']
-        ));
-
-        register_post_meta('hubpost', 'phone', array(
-            'type' => 'string',
-            'single' => true,
-            'show_in_rest' => true,
-            'sanitize_callback' => 'sanitize_text_field',
-            'auth_callback' => [$this, 'can_user_edit_hubpost']
-        ));
+                }
+            })
+        </script>
+        ';
     }
 }
 
